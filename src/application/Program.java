fe054634f4a2300322dc04f6e63ccf9dbb43ae4c
Giovani <giovanigithub@gmail.com>
2024-03-28 17:06:40 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		
+		System.out.println("\n=== TEST 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha wane");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+
 
 	}
 
